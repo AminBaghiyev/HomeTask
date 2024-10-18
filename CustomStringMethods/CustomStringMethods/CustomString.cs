c3e85153e626c1868f60fbbb53b036e662651946
Amin Baghiyev <aminib-ab205@code.edu.az>
2024-10-18 04:51:16 +0400

    CustomStringMethods task added

---
+++ CustomStringMethods/CustomStringMethods/CustomString.cs
@@ -0,0 +1,95 @@
+﻿using System.Text;
+
+namespace CustomStringMethods.Models;
+
+internal class CustomString
+{
+    public string Value { get; set; }
+
+    public CustomString(string value)
+    {
+        Value = value;
+    }
+
+    public string[] Split(char separator)
+    {
+        int splittedArrayLength = 1;
+        for (int i = 0; i < Value.Length; i++)
+        {
+            if (Value[i] == separator) splittedArrayLength++;
+            
+        }
+
+        string[] splittedString = new string[splittedArrayLength];
+        int indexOfSplittedString = 0;
+        StringBuilder str = new();
+        for (int i = 0; i < Value.Length; i++)
+        {
+            if (Value[i] == separator) {
+                splittedString[indexOfSplittedString] = str.ToString();
+                indexOfSplittedString++;
+                str.Clear();
+                continue;
+            }
+            str.Append(Value[i]);
+        }
+        splittedString[indexOfSplittedString] = str.ToString();
+
+        return splittedString;
+    }
+
+    public string Replace(char oldChar, char newChar)
+    {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < Value.Length; i++)
+        {
+            if (Value[i] != oldChar) str.Append(Value[i]);
+            else str.Append(newChar);
+        }
+
+        return str.ToString();
+    }
+
+    public string Replace(string oldString, string newString)
+    {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < Value.Length; i++)
+        {
+            if (Value.Length - oldString.Length >= i && Value.Substring(i, oldString.Length) == oldString)
+            {
+                str.Append(newString);
+                i += (oldString.Length - 1);
+            } else str.Append(Value[i]);
+        }
+
+        return str.ToString();
+    }
+
+    // burada ozum startIndex olan yeni metod yazmaq yerine ozune yazdim
+    public int LastIndexOf (string value, int startIndex = 0)
+    {
+        if (Value.Length - value.Length < startIndex || startIndex == 0) startIndex = Value.Length - value.Length;
+
+        for (int i = startIndex; i >= 0; i--)
+        {
+            if (Value.Substring(i, value.Length) == value) return i;
+        }
+
+        return -1;
+    }
+
+    // burada ozum startIndex olan yeni metod yazmaq yerine ozune yazdim
+    public int LastIndexOf(char value, int startIndex = 0)
+    {
+        if (Value.Length - 1 < startIndex || startIndex == 0) startIndex = Value.Length - 1;
+
+        for (int i = startIndex; i >= 0; i--)
+        {
+            if (Value[i] == value) return i;
+        }
+
+        return -1;
+    }
+}
---
+++ CustomStringMethods/CustomStringMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using CustomStringMethods.Models;
+
+namespace CustomStringMethods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CustomString str = new("attest");
+
+        Console.WriteLine(str.Split('t').Length);
+
+        foreach (var st in str.Split('t'))
+        {
+            Console.WriteLine(st);
+        }
+
+        Console.WriteLine("--");
+
+        string test = "attest";
+        Console.WriteLine(test.Split('t').Length);
+        foreach (var sr in test.Split('t'))
+        {
+            Console.WriteLine(sr);
+        }
+
+        Console.WriteLine("--");
+
+        Console.WriteLine(str.Replace("te", "a"));
+        Console.WriteLine("--");
+        Console.WriteLine(test.Replace("te", "a"));
+
+        Console.WriteLine("\n----\n");
+        CustomString text11 = new("Hello world, hello, rold, world");
+        Console.WriteLine(text11.LastIndexOf('r', 19));
+        string text12 = "Hello world, hello, rold, world";
+        Console.WriteLine(text12.LastIndexOf('r', 19));
+    }
+}
